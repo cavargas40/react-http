531c2742e30c577fff58a8684be07b00eb2ba89f
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-02-23 14:32:08 -0500

    sending a DELETE request

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -10,7 +10,10 @@ class FullPost extends Component {
 
   componentDidUpdate() {
     if (this.props.id) {
-      if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+      if (
+        !this.state.loadedPost ||
+        (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)
+      ) {
         axios
           .get(`http://jsonplaceholder.typicode.com/posts/${this.props.id}`)
           .then(response => {
@@ -19,6 +22,12 @@ class FullPost extends Component {
       }
     }
   }
+
+  deletePostHandler = () => {
+    axios
+      .delete(`http://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+      .then(response => console.log(response));
+  };
 
   render() {
     let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
@@ -31,7 +40,9 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button className="Delete">Delete</button>
+            <button className="Delete" onClick={this.deletePostHandler}>
+              Delete
+            </button>
           </div>
         </div>
       );
