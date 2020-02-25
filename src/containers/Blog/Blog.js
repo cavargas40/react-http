5dea140c0d25355e45c30e20fa9d66f49b43d9a6
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-02-24 20:16:01 -0500

    proper links of react-router-dom & styling the active route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
   render() {
-    
     return (
       <div className="Blog">
         <header>
           <nav>
             <ul>
               <li>
-                <a href="/">Home</a>
+                <NavLink to="/" exact activeClassName="active">
+                  Home
+                </NavLink>
               </li>
               <li>
-                <a href="/new-post">New Post</a>
+                <NavLink
+                  to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quick-submit=true'
+                  }}
+                >
+                  New Post
+                </NavLink>
               </li>
             </ul>
           </nav>
         </header>
-        <Posts />
-        {/* <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section> */}
+        <Route path="/" exact component={Posts} />
+        <Route path="/new-post" exact component={NewPost} />
       </div>
     );
   }
